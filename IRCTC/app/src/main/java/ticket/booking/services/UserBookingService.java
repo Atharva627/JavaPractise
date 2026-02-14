82afb69fc9d998c6a2622e4a4f6feb35daf9f020
Atharva627 <atharva.talaghatti@ats-group.com>
2026-02-14 17:32:55 +0530

    Ticket Booking App

--- IRCTC/app/src/main/java/ticket/booking/services/UserBookingService.java
+++ IRCTC/app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,20 +2,24 @@ package ticket.booking.services;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.Ticket;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
 
 import javax.imageio.IIOException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
 
     private List<User> userList;
+    private List<Ticket> userTicketsList;
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final String USERS_PATH="../localDB/users.json";
+    private static final String USERS_PATH="app/src/main/java/ticket/booking/localDB/users.json";
 
     public UserBookingService(User user) throws IOException {
         this.user = user;
@@ -23,4 +27,43 @@ public class UserBookingService {
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
         });
     }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+
+    }
+
+    public Boolean signUp(User user1){
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException{
+        File userFile = new File (USERS_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBooking(){
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId){
+        try {
+            //todo: Complete this function
+
+
+            return Boolean.TRUE;
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
 }
+
